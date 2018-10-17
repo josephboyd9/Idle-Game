ae9de88d6c76285a42c824e3e35068ed61a95857
Joseph Boyd <josephboyd9@gmail.com>
2018-10-17 19:58:23 +1300

    Still does almost nothing, but now it's in a pretty grid while it does it! :D

--- game.js
+++ game.js
@@ -1,29 +1,42 @@
-var v0 = 0;
-var v1 = 1;
-var boost = 1.0;
+var V0 = 0;
+var V1 = 0;
+var BOOST = 1.0;
+
+function incV0(amount) {
+    if (V0 < 100 && (V0 + amount) >= 100)
+    {
+        setUpgradeHidden(false);
+    }
+    V0 += amount
+}
 
 function shitpost() {
-    v0++;
-    boost += 0.01;
+    incV0(1);
+    BOOST += 0.01;
     updateDisplay()
 }
 
 function upgradeClicker() {
-    v1 += 1;
+    V1 += 1;
     updateDisplay()
 }
 
 function tick() {
-    v0 += v1 * boost;
-    var new_boost = boost - 0.1;
-    boost = ((new_boost > 1.0) ? new_boost : 1.0);
+    incV0(V1 * BOOST);
+    var new_boost = BOOST - 0.1;
+    BOOST = ((new_boost > 1.0) ? new_boost : 1.0);
     updateDisplay()
 }
 
 function updateDisplay() {
-    document.getElementById("v0").textContent = v0.toString();
-    document.getElementById("v1").innerText = v1.toString();
-    document.getElementById("boost").textContent = ((boost - 1) * 100).toString();
+    document.getElementById("v0").textContent = V0.toString();
+    document.getElementById("v1").innerText = V1.toString();
+    document.getElementById("boost").textContent = (+((BOOST - 1) * 100).toFixed(2)).toString();
+}
+
+function setUpgradeHidden(val) {
+    document.getElementById("clickerUpgrade").hidden = val;
+    document.getElementById("upgradeDesc").hidden = val;
 }
 
 var tickTrigger = setInterval(tick, 1000);
