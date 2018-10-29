c5d6c6fcacbd981a4661c6ccd6042b3af328fdc5
Joseph Boyd <josephboyd9@gmail.com>
2018-10-29 19:57:46 +1300

    Gave generic value names for easier coding. Added 2 layers of buttons. Looks tidy and technically works, still no real game as everything unlocks instantly

--- game.js
+++ game.js
@@ -1,42 +1,44 @@
-var V0 = 0;
-var V1 = 0;
+var V = [0, 0, 0, 0, 0];
 var BOOST = 1.0;
 
-function incV0(amount) {
-    if (V0 < 100 && (V0 + amount) >= 100)
-    {
-        setUpgradeHidden(false);
-    }
-    V0 += amount
-}
-
-function shitpost() {
-    incV0(1);
-    BOOST += 0.01;
+function incV(ind) {
+    //if (V[ind] < 100 && (V[ind] + amount) >= 100)
+    //{
+    //    setUpgradeHidden(false);
+    //}
+    V[ind]++;
     updateDisplay()
 }
 
-function upgradeClicker() {
-    V1 += 1;
+function boost() {
+    BOOST += 0.1;
     updateDisplay()
 }
 
 function tick() {
-    incV0(V1 * BOOST);
+    for(var i = 0; i < V.length; i++)
+    {
+        if(i + 1 < V.length) {
+            V[i] += V[i + 1] * BOOST ;
+        }
+    }
     var new_boost = BOOST - 0.1;
     BOOST = ((new_boost > 1.0) ? new_boost : 1.0);
-    updateDisplay()
+    setVHidden(2, false);
+    updateDisplay();
 }
 
 function updateDisplay() {
-    document.getElementById("v0").textContent = V0.toString();
-    document.getElementById("v1").innerText = V1.toString();
+    for(var i = 0; i < V.length; i++)
+    {
+        document.getElementById("v".concat(i.toString())).textContent = V[i].toString();
+    }
     document.getElementById("boost").textContent = (+((BOOST - 1) * 100).toFixed(2)).toString();
 }
 
-function setUpgradeHidden(val) {
-    document.getElementById("clickerUpgrade").hidden = val;
-    document.getElementById("upgradeDesc").hidden = val;
+function setVHidden(ind, val) {
+    document.getElementById("v".concat(ind.toString(), "Button")).hidden = val;
+    document.getElementById("v".concat(ind.toString(), "Desc")).hidden = val;
 }
 
 var tickTrigger = setInterval(tick, 1000);
